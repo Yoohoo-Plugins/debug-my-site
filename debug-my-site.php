2147f22d5a3e9d53fe7ce9ed6644a9abc73f4cb2
Andrew Lima <andrew@arctek.co.za>
2017-04-06 18:49:57 +0200

    Update debug-my-site.php

--- debug-my-site.php
+++ debug-my-site.php
@@ -23,6 +23,10 @@
 *
 * CHANGE LOG
 *
+* 1.0.3 - 06/04/2017
+* Improved coding standards
+* Enhancement: Ability for users to download debug information
+*
 * 1.0.2 - 28/03/2017
 * Enhancement: include JQuery version in debug information. (Works only with default JQuery that is loaded from WordPress)
 *
